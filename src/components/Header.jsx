8ac7635f60580fb2ba99d489bec7b52c3a7c5351
Hilcbek <balemayehu07@gmail.com>
2024-07-09 12:48:57 +0300

    addedd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="w-full bg-headerPrimary sm:after:flex after:hidden relative after:absolute after:right-[190px] after:transform after:rotate-[20deg] after:top-0 after:content-[''] after:w-[2px] after:h-full after:bg-white text-white text-sm">
       <div className="px-6 sm:flex-row flex-col flex items-center py-2 justify-between w-full">
-        <div className="flex group cursor-pointer items-center justify-start gap-2">
+        <div className="flex group cursor-pointer sm:text-left text-center items-center justify-start gap-2">
           <h1>Are you ready to grow up your business? Contact Us</h1>
           <FaArrowRight className="group-hover:translate-x-1 transition duration-150 ease-linear cursor-pointer" />
         </div>
